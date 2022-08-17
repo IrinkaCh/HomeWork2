124b5dd969bcd406dfde96a7cda7a4315c9a397c
IrinkaCh <123irina00@mail.ru>
2022-08-17 21:03:18 +0500

    Task 001

---
+++ task001/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите два числа ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(FindStepenNumber(A));
+
+double FindStepenNumber(int A)
+{
+double result = Math.Pow(A, B);
+return result;
+}
